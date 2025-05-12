a707b4f1e141baa774c2b9905ddb2362823c795b
DESKTOP-6QFCR0F\user <jasonlipreso@gmail.com>
2025-05-12 14:03:35 +0800

    Pagination added

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,14 @@ use Illuminate\Support\Facades\Route;
 use Jazer\Forms\Http\Controllers\Create\FormSubmission;
 use Jazer\Forms\Http\Controllers\Update\SeenSubmission;
 use Jazer\Forms\Http\Controllers\Update\StatusSubmission;
+use Jazer\Forms\Http\Controllers\Fetch\Paginate;
 
 Route::group(['prefix' => 'api'], function () {
     Route::group(['prefix' => 'forms'], function () {
         Route::get('formsubmission', [FormSubmission::class, 'submit']);
         Route::get('seensubmission', [SeenSubmission::class, 'seen']);
         Route::get('statussubmission', [StatusSubmission::class, 'update']);
+        Route::get('paginatesubmissions', [Paginate::class, 'paginate']);
     });
 });
 
